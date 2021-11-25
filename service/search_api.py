a4a10a71225aa6e630719cfea30866e3f50f8e14
Sens3ii <densteeper@gmail.com>
2021-11-26 00:06:23 +0600

    refac: search api

--- service/search_api.py
+++ service/search_api.py
@@ -1,45 +1,53 @@
 import requests
 import json
 from bs4 import BeautifulSoup
+import urllib.request
+from inscriptis import get_text
 
 
-def save_to_json(data, filename="data/data.json", mode="w"):
-    with open(file=filename, mode=mode, encoding='utf-8') as outfile:
+def data_to_file(data, path, mode_="w"):
+    print(f"[DATA TO FILE] data/{path}")
+    with open(file=f"data/{path}", mode=mode_, encoding='utf-8') as outfile:
         outfile.write(data)
 
 
-def save_query_results(filename="data/data.json", query=""):
-    r = requests.get(
-        f'https://searx.roughs.ru/search?q={query}&format=json')
+def get_search_res(query):
+    print(f"[GET] Search results {query} ")
+    r = requests.get(f'https://searx.roughs.ru/search?q={query}&format=json')
     data_json = json.dumps(r.json(), ensure_ascii=False, indent=4)
-    save_to_json(data_json)
+    data_to_file(data_json, "search_page.json")
 
 
-def get_dict(filename="data/data.json"):
+def get_urls_from_search_page():
+    print(f"[GET] URLs from search results")
+    search_page_dict = json_file_to_dict("data/search_page.json")
+    urls = [res['url'] for res in search_page_dict["results"]]
+    data_json = json.dumps(urls, ensure_ascii=False, indent=4)
+    data_to_file(data_json, "urls.json")
+
+
+def json_file_to_dict(filename):
+    print(f"[CONVERT] JSON file to dict {filename}")
     with open(file=filename, mode='r', encoding='utf-8') as json_file:
-        json_dict = json.load(json_file)
-        return json_dict
+        dict_ = json.load(json_file)
+        return dict_
 
 
-def save_urls(filename="data/data.json"):
-    json_dict = get_dict(filename="data/data.json")
-    urls = [res['url'] for res in json_dict["results"]]
-    json_string = json.dumps(urls, ensure_ascii=False, indent=4)
-    save_to_json(json_string, filename="data/urls.json")
-
-
-def get_data_from_url(urls_path="data/urls.json"):
-    json_dict = get_dict(urls_path)
+def get_data_from_url():
+    print(f"[GET] Data from URLs")
+    urls_dict = json_file_to_dict("data/urls.json")
     data_list = []
-    for url in json_dict:
-        print(url)
-        r = requests.get(url).text
-        formatted = BeautifulSoup(r, 'html.parser').get_text()
-        data_list.append(formatted)
-    json_string = json.dumps(data_list, ensure_ascii=False, indent=4)
-    save_to_json(json_string, filename="data/pages.json")
+    for url in urls_dict:
+        print(f"[GET] Data from URL: {url}")
+        try:
+            html = urllib.request.urlopen(url).read().decode('utf-8')
+            text = get_text(html)
+            text = ' '.join(text.split())
+            data_to_file(text, "data.txt", "a")
+        except:
+            print(f"[ERROR] Data from URL: {url}")
 
 
-# save_query_results(query="Toyota")
-# save_urls()
+# get_search_res(query="Toyota")
+# get_urls_from_search_page()
 get_data_from_url()
